071f6ba3a55a3373692cc1ba42392370432cc5b3
Govind S B <b.s.dnivog@gmail.com>
2023-12-13 13:22:19 +0530

    working MVP

--- vectorize.py
+++ vectorize.py
@@ -1,14 +1,7 @@
+from uuid import uuid1
 import re
 import chromadb
-import uuid
-
-# reference : https://github.com/Govind-S-B/pdf-to-text-chroma-search
-
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('infgrad/stella-base-en-v2')
-
-def vectorize_sentence(string):
-    pass
+from chromadb.utils import embedding_functions
 
 def batch_process_test_data(document):
     # Find all content within the backticks
@@ -19,24 +12,34 @@ def batch_process_test_data(document):
     
     return content
 
-client = chromadb.PersistentClient(path="./db")
-collection = client.create_collection(name="faq_data")
+# DB INIT
+
+client = chromadb.EphemeralClient()
+embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="infgrad/stella-base-en-v2",)
+collection = client.get_or_create_collection(name="faq", embedding_function=embedding_func)
+
+# WRITE SECTION
 
 file_content = open("faq_test_data.txt").read()
 data_list = batch_process_test_data(file_content)
 
-documents_list = []
-embeddings_list = []
-ids_list = []
-
-for data_chunk in data_list:
-    documents_list.append(data_chunk)
-    vector = model.encode(data_chunk)
-    embeddings_list.append(vector)
-    ids_list.append(str(uuid.uuid1()))
+documents_list = data_list
+ids_list = [str(uuid1()) for i in range(0,len(data_list))]
 
 collection.add(
-    embeddings=embeddings_list,
-    documents=documents_list,
-    ids=ids_list
-)
+            ids=ids_list,
+            documents=documents_list,
+        )
+
+
+# READ SECTION
+
+query_qn = "i am stuck in my level"
+
+fetched_doc = collection.query(
+     query_texts=query_qn,
+     n_results=1,
+     include=["documents"]
+ )["documents"][0][0]
+
+print(fetched_doc)
